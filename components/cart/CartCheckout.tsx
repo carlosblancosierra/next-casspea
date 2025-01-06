15b1b07a82ce109178fceba730393a85214e74be
Carlos Blanco <carlosblancosierra@gmail.com>
2025-01-06 20:13:58 +0100

    Cart Checkout Style

--- components/cart/CartCheckout.tsx
+++ components/cart/CartCheckout.tsx
@@ -81,10 +81,10 @@ export default function CartCheckout() {
     };
 
     return (
-        <div className="space-y-4 px-5 mt-4">
+        <div className="space-y-4 mt-4">
             {/* Order Summary - Moved to top */}
-            <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
-                <p className="text-xl font-semibold text-gray-900 dark:text-white -mx-5">Order summary</p>
+            <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6 px-5">
+                <p className="text-xl font-semibold text-gray-900 dark:text-white">Order summary</p>
 
                 <div className="space-y-4">
                     {/* Original Price */}
@@ -173,7 +173,7 @@ export default function CartCheckout() {
                                             className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                                         >
                                             <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                             </svg>
                                             <span className="sr-only">Close modal</span>
                                         </button>
