6a642e808af4f4884ca003c41c7e5f053947e2f8
Carlos Blanco <carlosblancosierra@gmail.com>
2025-01-07 22:06:55 +0100

    Style - allergen button

--- components/product_detail/AllergenSelection.tsx
+++ components/product_detail/AllergenSelection.tsx
@@ -82,7 +82,7 @@ const AllergenSelection: React.FC<AllergenSelectionProps> = ({
                     className={({ active, checked }) =>
                         `${checked
                             ? 'bg-indigo-50 dark:bg-indigo-900 border-indigo-600'
-                            : 'bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-600'
+                            : 'border-gray-300 dark:border-gray-600'
                         }
                         relative rounded-lg border px-5 py-4 shadow-sm cursor-pointer flex focus:outline-none`
                     }
