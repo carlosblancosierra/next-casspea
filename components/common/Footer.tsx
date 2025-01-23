098a3a2e8a709329aeda87234311c2ad2b0833e5
cblanco-clintell <c.blanco@clintell.io>
2025-01-23 01:33:55 +0100

    Valentines Updates

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -4,12 +4,13 @@ export default function Footer() {
 	return (
 		<footer className='bg-gray-100 h-16 dark:bg-gray-800'>
 			<div className='h-full px-2'>
-				<div className='flex items-center justify-center h-full'>
+				<div className='flex flex-col items-center justify-center h-full gap-1'>
 					<p className='text-gray-400 text-xs'>
 						&copy; 2024 CassPea LTD. All rights reserved.
 					</p>
-					<p><Link href="/orders">Log In</Link></p>
-					{/* <p>Software by Carlos Blanco</p> */}
+					<p className='text-gray-400 text-xs'>
+						<Link href="/orders">Log In</Link>
+					</p>
 				</div>
 			</div>
 		</footer>
--- components/product_detail/ProductFormGeneral.tsx
+++ components/product_detail/ProductFormGeneral.tsx
@@ -42,11 +42,11 @@ const ProductFormGeneral: React.FC<ProductFormGeneralProps> = ({ product }) => {
     return (
         <form>
             <div className="mt-4">
-                <p className="text-2xl md:text-2xl tracking-tight mb-2 dark:text-white ">
+                <p className="text-xl md:text-2xl tracking-tight mb-2 dark:text-white ">
                     Shipping starts from February 5th to 7th, just in time for Valentine&apos;s Day! 
                     Pre-order now to ensure your gift arrives on time.</p>
-                <p className="text-2xl md:text-2xl tracking-tight mb-2 text-primary">
-                    Use Code <span className="font-bold">ValentinesPreorder</span> for 15% off until 31st January at midnight
+                <p className="text-lg md:text-2xl tracking-tight mb-2 text-primary">
+                    Use Code <span className="font-bold">ValentinesPreorder</span> in the cart for 15% off until 31st January at midnight
                 </p>
                 <label htmlFor="quantity" className="block text-sm font-medium text-gray-700 dark:text-gray-200">
                     Quantity
