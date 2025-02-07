1b45b126ed5b77d42354e51d19ee3f6b07422896
Carlos Blanco <carlosblancosierra@gmail.com>
2025-02-07 22:51:04 +0100

    Feat - Remove Chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,21 +63,6 @@ export default function RootLayout({
             gtag('config', 'G-FBD3HVXC41');
           `}
         </Script>
-
-        <Script id="tawk-to" strategy="afterInteractive">
-          {`
-            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-            (function(){
-              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-              s1.async = true;
-              s1.src = 'https://embed.tawk.to/67a5f217825083258e11a954/1ijg3jbdp';
-              s1.charset = 'UTF-8';
-              s1.setAttribute('crossorigin', '*');
-              s0.parentNode.insertBefore(s1, s0);
-            })();
-          `}
-        </Script>
-
         <Provider>
           <Setup />
           <HeartEffect />
