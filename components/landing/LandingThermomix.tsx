59fe9417ad7a393e3fa5dc93ddae58c5e477662e
Carlos Blanco <carlosblancosierra@gmail.com>
2025-08-16 15:44:13 +0200

    Feat - Remove Social Proof

--- components/landing/LandingThermomix.tsx
+++ components/landing/LandingThermomix.tsx
@@ -67,7 +67,6 @@ export default function LandingThermomix() {
       <HowToEnterSection />
       <StoryShareSection />
       <BenefitsSection />
-      <SocialProofSection />
       <PrizeDetailsSection />
       <KeyTermsSection />
       <TermsSection />
