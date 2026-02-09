6f4962ad8691aad7afaa80eaeeeb855462ccc226
Carlos Blanco <carlosblancosierra@gmail.com>
2026-02-09 23:31:03 +0100

    Fix - Shipping Dates

--- components/checkout/CheckoutShippingOptions.tsx
+++ components/checkout/CheckoutShippingOptions.tsx
@@ -142,13 +142,19 @@ const CheckoutShippingOptions: React.FC<CheckoutShippingOptionsProps> = ({
     };
 
     const getEstimatedDeliveryDates = (minDays: number, maxDays: number) => {
-        // All orders will be dispatched on January 10th
-        const dispatchDate = new Date('2025-01-10');
-        const minDeliveryDate = addBusinessDays(dispatchDate, minDays);
-        const maxDeliveryDate = addBusinessDays(dispatchDate, maxDays);
+        const now = new Date();
+        // Get current hour in UK time (handles GMT/BST automatically)
+        const ukHour = parseInt(
+            new Intl.DateTimeFormat('en-GB', { hour: 'numeric', hour12: false, timeZone: 'Europe/London' }).format(now)
+        );
+        const SHIPPING_CUTOFF_HOUR = 10;
+        // If before 10 AM UK time, ship today; otherwise next business day
+        const shippingDate = ukHour < SHIPPING_CUTOFF_HOUR ? now : addBusinessDays(now, 1);
+        const minDeliveryDate = addBusinessDays(shippingDate, minDays);
+        const maxDeliveryDate = addBusinessDays(shippingDate, maxDays);
 
         return {
-            shipping: format(dispatchDate, 'EEE, d MMM'),
+            shipping: format(shippingDate, 'EEE, d MMM'),
             delivery: minDays === maxDays
                 ? format(minDeliveryDate, 'EEE, d MMM')
                 : `${format(minDeliveryDate, 'EEE, d MMM')} - ${format(maxDeliveryDate, 'EEE, d MMM')}`
@@ -280,7 +286,7 @@ const CheckoutShippingOptions: React.FC<CheckoutShippingOptionsProps> = ({
                     <div className="flex items-center justify-between mb-4">
                         <p className="text-sm text-primary-text dark:text-primary-text-light">
                             {deliveryType === 'shipping'
-                                ? 'All orders will be dispatched on January 10th. Delivery dates shown are from the dispatch date.'
+                                ? 'Choose your preferred shipping method below.'
                                 : 'Pick up your order from our Bedford Hill store. Choose a convenient time slot.'
                             }
                         </p>
@@ -365,7 +371,7 @@ const CheckoutShippingOptions: React.FC<CheckoutShippingOptionsProps> = ({
                                 <div className="mt-4 p-4 bg-yellow-50 dark:bg-yellow-900/20 border border-yellow-200 dark:border-yellow-800 rounded-lg">
                                     <div className="flex items-start">
                                         <svg className="w-5 h-5 text-yellow-600 dark:text-yellow-400 mt-0.5 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
-                                            <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd"/>
+                                            <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                                         </svg>
                                         <div>
                                             <h3 className="text-sm font-semibold text-yellow-800 dark:text-yellow-200">
